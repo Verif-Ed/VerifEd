ceec90367bb47b067c1e3d64638be5b014a93726
lokesh-katari <lokeshkatari921@outlook.com>
2024-06-06 16:44:48 +0530

    chaged module to dapp kit

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,27 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { WalletProvider } from "@suiet/wallet-kit";
+// import { WalletProvider } from "@suiet/wallet-kit";
+import "@mysten/dapp-kit/dist/index.css";
+
+import { SuiClientProvider, WalletProvider } from "@mysten/dapp-kit";
+import { getFullnodeUrl } from "@mysten/sui.js/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // import { ConnectModal } from "@mysten/wallet-kit";
+const queryClient = new QueryClient();
+const networks = {
+  localnet: { url: getFullnodeUrl("localnet") },
+  devnet: { url: getFullnodeUrl("devnet") },
+  testnet: { url: getFullnodeUrl("testnet") },
+  mainnet: { url: getFullnodeUrl("mainnet") },
+};
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <WalletProvider>
-      <App />
-    </WalletProvider>
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <SuiClientProvider networks={networks} defaultNetwork="devnet">
+      <WalletProvider>
+        <App />
+      </WalletProvider>
+    </SuiClientProvider>
+  </QueryClientProvider>
 );
